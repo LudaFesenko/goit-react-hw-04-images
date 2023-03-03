bdff297d0bfc848c4a29cde220f1cf3b5258cef5
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-03-03 12:42:59 +0200

    add Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,20 @@
+import { ColorRing } from 'react-loader-spinner';
+
+import { StyledLoader } from './Loader.styled';
+
+function Loader() {
+  return (
+    <StyledLoader>
+      <ColorRing
+        visible={true}
+        height="180"
+        width="180"
+        ariaLabel="blocks-loading"
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    </StyledLoader>
+  );
+}
+
+export default Loader;

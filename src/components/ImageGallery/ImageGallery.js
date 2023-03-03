10e42c182c025ba69018f190b69211f35cb8f3e9
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-03-03 12:42:18 +0200

    add ImageGallery

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+import ImageGalleryItem from 'components/ImageGalleryItem';
+import { ImageList } from './ImageGallery.styled';
+
+const ImageGallery = ({ images, onClickImg }) => {
+  return (
+    <ImageList>
+      {images.map(({ id, tags, webformatURL, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          src={webformatURL}
+          dataSrc={largeImageURL}
+          alt={tags}
+          onClickImg={onClickImg}
+        />
+      ))}
+    </ImageList>
+  );
+};
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      tags: PropTypes.string.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onClickImg: PropTypes.func.isRequired,
+};
+
+export default ImageGallery;
